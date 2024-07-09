db76ab0becc8ee82f8c551f7ffbc0c89adeaa19a
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-09 16:09:24 +0200

    fce compare_files

---
+++ task1.py
@@ -0,0 +1,22 @@
+FILE_1 = "file_1.txt"
+FILE_2 = "file_2.txt"
+
+def compare_files(f1_name, f2_name):
+    with open("file_1.txt", "r") as f1_text:
+        f1 = f1_text.readlines()
+    with open("file_2.txt", "r") as f2_text:
+        f2 = f2_text.readlines()
+
+    for line_1 in f1:
+        for line_2 in f2:
+            if line_1 != line_2:
+                pass
+            elif line_1 == line_2:
+                print("Text v obou souborech je stejny.")
+            else:
+                print("kontrola")
+
+compare_files(FILE_1, FILE_2)
+
+
+
