51e5883e55165df0059417275ab6c3524536da20
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-09 23:37:53 +0200

    teď už funguje
    - přidáno porovnání délky listů, teď už se tiskne vše i z 1. složky
    - předtím se mi netiskl posl. řádek z delší složky (1.složka)

--- task1.py
+++ task1.py
@@ -6,17 +6,21 @@ def compare_files(f1_name, f2_name):
         f1 = list(f1_text.readlines())
     with open("file_2.txt", "r", encoding="utf8") as f2_text:
         f2 = list(f2_text.readlines())
+    if len(f1) > len(f2):
+        f2.append(" " * (len(f1) - len(f2)))
+    if len(f2) > len(f1):
+        f1.append(" " * (len(f2) - len(f1)))
     txt1_diff = " "
     txt2_diff = " "
     for line_1,line_2 in zip(f1,f2):
         if line_1 not in f2:
             txt1_diff += line_1
-    for line_1,line_2 in zip(f1,f2):
         if line_2 not in f1:
             txt2_diff += line_2
 
     print(f"odlišný text v {FILE_1}: {txt1_diff}")
     print(f"odlišný text v {FILE_2}: {txt2_diff}")
+
 compare_files(FILE_1, FILE_2)
 
 
--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-FILE_1 = "file_1.txt"
-FILE_2 = "file_2.txt"
 
-def compare_files(f1_name, f2_name):
-    with open("file_1.txt", "r", encoding="utf8") as f1_text:
-        f1 = list(f1_text.readlines())
-        print(f1)
-    with open("file_2.txt", "r", encoding="utf8") as f2_text:
-        f2 = list(f2_text.readlines())
-        print(f2)
-    txt1_diff = " "
-    txt2_diff = " "
-    for line_1 in f1:
-        for line_2 in f2:
-            if line_1 != line_2:
-                txt1_diff += line_1
-                txt2_diff += line_2
 
-    print(f"odlišný text v {FILE_1}: {txt1_diff}")
-    print(f"odlišný text v {FILE_2}: {txt2_diff}")
-compare_files(FILE_1, FILE_2)
