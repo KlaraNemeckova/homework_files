cbb6fbf3ac3aadf70822d089113df1902ff39865
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-10 10:33:53 +0200

    task2
    funkce - kostra

---
+++ task2.py
@@ -0,0 +1,17 @@
+FILE_INPUT = "složka1.txt"
+FILE_OUTPUT = open("složka2.txt", "w")
+
+def char (file_1, file_2):
+    pass
+
+def lines (file_1, file_2):
+    pass
+
+def vowels (file_1, file_2):
+    pass
+
+def consonants (file_1, file_2):
+    pass
+
+def digits (file_1, file_2):
+    pass
