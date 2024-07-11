6d8958632a4a3b6d56659ec9871167fa2ebc29b9
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-11 20:05:38 +0200

    task5b
    využití RegEx, aby se nepočítaly kusy slov

---
+++ task5b.py
@@ -0,0 +1,26 @@
+FILE = "f_original.txt"
+
+import re                       # regular expression
+
+word = input("Hledané slovo: ")
+
+
+def word_count(file_name):
+    with (open(file_name, "r+", encoding="utf-8") as f):
+        text = f.read().split()
+        counter = 0
+        try:
+            for line in text:
+                n = len(re.findall(r"\b"+ word + r"\b", line))   # ---> jen celé slovo
+                counter += n
+                print(counter)
+
+
+            #if counter != 0:
+                #print(f"´{word}´ se v textu vyskytuje {counter}x.")
+            #else:
+                #print("Zadané slovo se v textu nenachází.")
+        except:
+            print("Chyba při načítání souboru")
+
+word_count(FILE)
