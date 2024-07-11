78439f133e437438abf16882d51b61c37d97a1a8
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-11 17:34:49 +0200

    vytvoření funkce

--- task4.py
+++ task4.py
@@ -1,9 +1,14 @@
-FILE = "f_original.txt"
+FILE = "longest.txt"
 
 
 def longest_line(file_name):
-    with open(file_name, "r+", encoding="utf-8"):
-        pass
-
+    with open(file_name, "r+", encoding="utf-8") as f:
+        data = f.readlines()
+        new_line = ""
+        for line in data:
+            if len(line) > len(new_line):
+                print(line)
+                new_line = line
+            print(len(new_line) -1)
 
 longest_line(FILE)
