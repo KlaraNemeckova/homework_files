5b7e7dfeddaccb86f58774e7cd692568010054a7
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-11 16:04:15 +0200

    def delete_last_line()

--- task3.py
+++ task3.py
@@ -1,14 +1,28 @@
+ORIG_FILE = "f_original.txt"
+
+
 def get_last_line(file_name):
     with open(file_name, "r+", encoding="utf-8") as f1:
-        data = list(f1.readlines())
+        data = f1.readlines()
         result = []
         for line in range(len(data)):
             if line == len(data) -1:
                 result += data[line]
-                print(data[line])
 
 
-get_last_line("f_original.txt")
+def delete_last_line(file_name):
+    with open(file_name, "r+", encoding="utf-8") as f1:
+        data = f1.readlines()
+        f1.seek(0)                   # vrátí kurzor na začátek
+        f1.truncate()
+        f1.writelines(data[:-1])
+
+
+
+get_last_line(ORIG_FILE)
+delete_last_line(ORIG_FILE)
+
+
 
 #with open("f_new.txt", "w", encoding="utf-8") as f2:
 # f2.writelines(line)
