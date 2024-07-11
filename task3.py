a3ac00362157f895f8fc42eb235b174a0b28ea4f
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-11 16:32:26 +0200

    sloučení do jedné funkce

--- task3.py
+++ task3.py
@@ -1,30 +1,20 @@
 ORIG_FILE = "f_original.txt"
 
 
-def get_last_line(file_name):
-    with open(file_name, "r+", encoding="utf-8") as f1:
-        data = f1.readlines()
-        result = []
-        for line in range(len(data)):
-            if line == len(data) -1:
-                result += data[line]
-
-
 def delete_last_line(file_name):
-    with open(file_name, "r+", encoding="utf-8") as f1:
+    with open(file_name, "r+", encoding="utf-8") as f1, open("f_new.txt", "w", encoding="utf-8") as f2:
         data = f1.readlines()
         f1.seek(0)                   # vrátí kurzor na začátek
         f1.truncate()
         f1.writelines(data[:-1])
+        f2.writelines(data[:-1])
 
 
-
-get_last_line(ORIG_FILE)
 delete_last_line(ORIG_FILE)
 
 
 
-#with open("f_new.txt", "w", encoding="utf-8") as f2:
-# f2.writelines(line)
+
+
 
 
