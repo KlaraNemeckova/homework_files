2f5c9260920afda875cb32b6dbf5a914b03cfa79
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-12 16:58:28 +0200

    bonus task

---
+++ Employees_app.py
@@ -0,0 +1,23 @@
+# načtení seznamu zaměstnanců
+
+
+# editing data
+
+
+# deleting employee
+
+
+# searching by last name
+
+
+# output - age, name(starting by spec. letter)
+
+
+# save info to a new file
+
+
+# ukončit aplikaci a uložit aktualizovaná data
+
+
+
+
