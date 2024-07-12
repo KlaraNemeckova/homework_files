4703ac24d062616d095907c39a8ed6bb028dc0bc
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-12 15:37:00 +0200

    task6 - hotovo
    RegEx
    oprava podmínky if

--- task6.py
+++ task6.py
@@ -1,5 +1,7 @@
 FILE = "file_3.txt"
 
+import re
+
 word = input("Jaké slovo chcete nahradit?: ")
 new_word = input("Nové slovo: ")
 
@@ -8,12 +10,12 @@ def replace(file_name):
     with open(file_name, "r+", encoding="utf-8") as f:
         data = f.read()
         try:
-            new_data = data.replace(word, new_word)
+            new_data = re.sub(r"\b" + word + r"\b", new_word, data)
             f.seek(0)
             f.truncate()
             f.write(new_data)
-            #if word not in f:
-                #print("Toto slovo se v textu nevyskytuje.")
+            if word not in data:
+                print("Toto slovo se v textu nevyskytuje.")
         except FileNotFoundError:
             print("Chyba při načítání souboru")
 
