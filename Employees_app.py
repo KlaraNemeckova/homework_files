fbe140022df16372237bb386003aa312177ac159
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-12 18:05:28 +0200

    funkce main
    plus názvy ostatních funkcí

--- Employees_app.py
+++ Employees_app.py
@@ -1,23 +1,64 @@
-# načtení seznamu zaměstnanců
+FILE = "employees.txt"
 
 
-# editing data
+def load_data():
+    pass
 
 
-# deleting employee
+def edit_list():
+    pass
 
 
-# searching by last name
+def add_employee():
+    pass
 
 
-# output - age, name(starting by spec. letter)
+def delete_employee():
+    pass
 
 
-# save info to a new file
+def search_by_name():
+    pass
 
 
-# ukončit aplikaci a uložit aktualizovaná data
+def select():
+    pass
 
+
+def save_data():
+    pass
+
+
+def main(file_name):
+    while True:
+        print("""--MENU-- \n1 = načíst data ze souboru \n2 = editace \n3 = přidat zaměstnance \n4 = smazat zaměstnance 
+              \n5 = vyhledat podle příjmení \n6 = vybrat zaměstnance dle věku nebo počátečního písmena
+              \n7 = uložit změny \n8 = ukončit program""")
+
+        choice = input("Vyberte možnost: ")
+
+        if choice == "1":
+            load_data()
+        elif choice == "2":
+            edit_list()
+        elif choice == "3":
+            add_employee()
+        elif choice == "4":
+            delete_employee()
+        elif choice == "5":
+            search_by_name()
+        elif choice == "6":
+            select()
+        elif choice == "7":
+            save_data()
+        elif choice == "8":
+            # save_data()
+            break
+        else:
+            print("Neplatná volba")
+
+
+main(FILE)
 
 
 
