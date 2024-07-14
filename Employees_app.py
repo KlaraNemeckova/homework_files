e4494348c3fa4948c63980e0dec6d840ce25a6fc
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-07-14 23:57:50 +0200

    funkce search_by_name

--- Employees_app.py
+++ Employees_app.py
@@ -6,12 +6,18 @@ FILE = "employees.txt"
 def load_data(file_name):
     with open(file_name, "r+") as file:
         employee_list = json.load(file)
-        for k, v in employee_list.items():
+        for k, v in employee_list.items():           # for k1 in employee_list.keys():
+                                                     # print(k1)
+                                                     #print(employee_list[k1])
             print(k, v)
+    # main()
 
 
 def edit_list():
     pass
+
+    # main()
+
 
 
 def add_employee(file_name):
@@ -23,30 +29,42 @@ def add_employee(file_name):
         my_values = [input("first name: "), input("last name: "), input("age: "), input("ID: "), input("email: ")]
         for key, value in zip(my_keys, my_values):
             new_dict[key] = value
-        new_employee[input("")] = new_dict
+        new_employee[input("")] = new_dict           # chyba...(zdvojený dictionary)
         employee_list.update(new_employee)
         file.write(json.dumps(employee_list))
+        # main()
 
 
 def delete_employee(file_name):
     with open(file_name, "r+") as file:
         employee_list = json.load(file)
-        print(employee_list)
-        del employee_list[input("číslo zaměstnance, kterého chcete smazat: ")]
-        print(employee_list)
+        del employee_list[input("Kterého zaměstnance chcete smazat? (´emp_´):  ")]
         file.write(json.dumps(employee_list))
+        # main()
 
 
-def search_by_name():
-    pass
+def search_by_name(file_name):
+    with open(file_name, "r+") as file:
+        employee_list = json.load(file)
+        last_name = input("Příjmení zaměstnance: ")
+        try:
+            for k1 in employee_list.keys():
+                if last_name in employee_list[k1].values():
+                    print(k1, employee_list[k1])
+        except:
+            print("Nenalezeno")
+
+       # main()
 
 
 def select():
     pass
+    # main()
 
 
 def save_data():
     pass
+    # main()
 
 
 def main(file_name):
@@ -64,7 +82,7 @@ def main(file_name):
         elif choice == "3":
             add_employee(FILE)
         elif choice == "4":
-            delete_employee()
+            delete_employee(FILE)
         elif choice == "5":
             search_by_name()
         elif choice == "6":
--- test.py
+++ test.py
@@ -3,13 +3,19 @@ import json
 FILE = "employees.txt"
 
 
-def delete_employee(file_name):
+def search_by_name(file_name):
     with open(file_name, "r+") as file:
         employee_list = json.load(file)
+        for k1 in employee_list.keys():
+            for k2,v in zip(employee_list[k1].values(),employee_list[k2].values()):
+                employee_list[k2.lower()] = v.lower()
         print(employee_list)
-        del employee_list[input("číslo zaměstnance, kterého chcete smazat: ")]
-        print(employee_list)
-        file.write(json.dumps(employee_list))
+        last_name = input("Příjmení zaměstnance: ")
+        try:
+            for k1 in employee_list.keys():
+                if last_name in employee_list[k1].values():
+                    print(k1, employee_list[k1])
+        except:
+            print("Nenalezeno")
 
-
-delete_employee(FILE)
+search_by_name(FILE)
